b5e2211bc5aeb10dda6e4859155a2dc519edc52a
Josh Moscoe <jmoscoe@manifestcorp.com>
2022-01-11 12:22:24 -0500

    starting graphql querier

---
+++ src/main/java/com/manifestcorp/onsquerier/OnsQuerierApplication.java
@@ -0,0 +1,13 @@
+package com.manifestcorp.onsquerier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnsQuerierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnsQuerierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manifestcorp/onsquerier/OnsQuerierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manifestcorp.onsquerier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnsQuerierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
